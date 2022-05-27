8516f0eab325b65a40c6b464ced9f2bd01f18535
colin.d.wagner <colin.d.wagner@gmail.com>
2022-05-27 12:39:25 -0500

    a few changes

--- 38_workout_tracker/main.py
+++ 38_workout_tracker/main.py
@@ -1,8 +1,10 @@
 import requests
 import datetime as dt
+import os
 
-APP_ID = "34ff5e85"
-API_KEY = "b40466b3f7eaee2081c7b2861be3d135"
+APP_ID = os.environ["APP_ID"]
+API_KEY = os.environ["API_KEY"]
+BEARER_TOKEN = os.environ["BEARER_TOKEN"]
 
 HEADERS = {
     "x-app-id": APP_ID,
@@ -33,6 +35,9 @@ current_time = dt.datetime.now().strftime("%X")
 sheet_endpoint = "https://api.sheety.co/1f4c9ffa2aa5ad343c8c27d52f0aeec7/workouts/sheet1"
 
 
+bearer_header = {
+    "Authorization": f"Bearer {BEARER_TOKEN}"
+}
 for exercise in exercises_data:
     sheet_body = {
         "sheet1": {
@@ -43,5 +48,5 @@ for exercise in exercises_data:
             "calories": exercise["nf_calories"],
         }
     }
-    sheet_resp = requests.post(url=sheet_endpoint, json=sheet_body, verify=False)
+    sheet_resp = requests.post(url=sheet_endpoint, json=sheet_body, headers= bearer_header, verify=False)
     print(sheet_resp.text)
