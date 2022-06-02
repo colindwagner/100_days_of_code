03d10928e160318dfdc342cee2b7abd5f3b7752c
colin.d.wagner <colin.d.wagner@gmail.com>
2022-06-02 07:41:50 -0500

    added sms notify

--- 39_flight_finder/notification_manager.py
+++ 39_flight_finder/notification_manager.py
@@ -1,3 +1,22 @@
+from twilio.rest import Client
+import os
+
+TWILIO_SID = 'ACc9604759e58a2cc4a1570d9fdd82724c' 
+TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH")
+TWILIO_VIRTUAL_NUMBER = "+17652075906"
+TWILIO_VERIFIED_NUMBER = "+18329935448"
+
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+
+    def __init__(self):
+        self.client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    def send_sms(self, message):
+        message = self.client.messages.create(
+            body=message,
+            from_=TWILIO_VIRTUAL_NUMBER,
+            to=TWILIO_VERIFIED_NUMBER,
+        )
+        # Prints if successfully sent.
+        print(message.sid)
